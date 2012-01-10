48579adef6dfe891148635711eb329ae293f0e3b
avto25 <maxcorp@bk.ru>
2012-01-10 11:01:31 +0400

    aaa

--- app/config/main.php
+++ app/config/main.php
@@ -171,7 +171,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=wecms',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'sko62267',
+			'password' => '',
 			'charset' => 'utf8',
 			
 			// включаем профайлер
