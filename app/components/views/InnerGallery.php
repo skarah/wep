2885634a78ff46380f47bec624cc0c14bdf22c94
Skorohodov Denis <skarah@mail.ru>
2012-01-10 11:19:35 +0400

    new changes

--- app/components/views/InnerGallery.php
+++
@@ -1,6 +0,0 @@
-		<span style="display:block"><?
-		for($i=0;$i<sizeof($images);$i++)
-		{
-			?><img src="<?=Yii::app()->params['thumbDir'].Yii::app()->params['thumbPrefix'].$images[$i]->file?>" height="75"><?
-		}
-		?></span>
